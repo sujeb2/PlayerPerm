24d95e0446cb38c4000d6aad1ee4349e8b994452
songro_ <sdbb2q@gmail.com>
2023-03-11 16:18:41 +0900

    Changed to local

---
+++ src/com/songro/commands/Friend.java
@@ -0,0 +1,150 @@
+package com.songro.commands;
+
+import com.songro.data.FD;
+import com.songro.handler.Timer;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.*;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+
+public class Friend implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        try {
+            Player player = (Player) sender;
+            if (strings.length == 0) {
+                player.sendMessage(ChatColor.YELLOW + "[PlayerPerms] 플레이어 이름이 없습니다.");
+                player.sendMessage(ChatColor.GREEN + "[PlayerPerms] 명령어 사용 방법: /<commands> 추가 (Player)");
+            }
+            switch (strings[0]) {
+                case "추가":
+                    Player f = Bukkit.getPlayer(strings[1]);
+                    if (strings.length == 1) {
+                        player.sendMessage(ChatColor.GREEN + "[PlayerPerms] 명령어 사용 방법: /<commands> 추가 (Player)");
+                    }
+                    /*
+                    온라인이 아닐때
+                     */
+                    else if (!Bukkit.getOnlinePlayers().contains(f)) {
+                        player.sendMessage(ChatColor.YELLOW + "[PlayerPerms] 등록할 친구가 온라인이 아닙니다.");
+                    }
+                    /*
+                    null 일때
+                     */
+                    else if (f == null) {
+                        player.sendMessage(ChatColor.RED + "[PlayerPerms] info is null");
+                    } else if (f.equals(player)) {
+                        player.sendMessage(ChatColor.YELLOW + "[PlayerPerms] 자신에게 친구추가를 보낼수 없습니다!");
+                    } else {
+                        req(player, f);
+                    }
+                    break;
+                case "ㅊ":
+                    Player f2 = Bukkit.getPlayer(strings[1]);
+                    if (strings.length == 1) {
+                        player.sendMessage(ChatColor.GREEN + "[PlayerPerms] 명령어 사용 방법: /<commands> 추가 (Player)");
+                    }
+                    /*
+                    온라인이 아닐때
+                     */
+                    else if (!Bukkit.getOnlinePlayers().contains(f2)) {
+                        player.sendMessage(ChatColor.YELLOW + "[PlayerPerms] 등록할 친구가 온라인이 아닙니다.");
+                    }
+                    /*
+                    null 일때
+                     */
+                    else if (f2 == null) {
+                        player.sendMessage(ChatColor.RED + "[PlayerPerms] info is null");
+                    } else if (f2.equals(player)) {
+                        player.sendMessage(ChatColor.YELLOW + "[PlayerPerms] 자신에게 친구추가를 보낼수 없습니다!");
+                    } else {
+                        req2(player, f2);
+                    }
+                    break;
+                case "add":
+                    Player f3 = Bukkit.getPlayer(strings[1]);
+                    if (strings.length == 1) {
+                        player.sendMessage(ChatColor.GREEN + "[PlayerPerms] 명령어 사용 방법: /<commands> 추가 (Player)");
+                    }
+                    /*
+                    온라인이 아닐때
+                     */
+                    else if (!Bukkit.getOnlinePlayers().contains(f3)) {
+                        player.sendMessage(ChatColor.YELLOW + "[PlayerPerms] 등록할 친구가 온라인이 아닙니다!");
+                    }
+                    /*
+                    null 일때
+                     */
+                    else if (f3 == null) {
+                        player.sendMessage(ChatColor.RED + "[PlayerPerms] info is null");
+                    } else if (f3.equals(player)) {
+                        player.sendMessage(ChatColor.YELLOW + "[PlayerPerms] 자신에게 친구추가를 보낼수 없습니다.");
+                    } else {
+                        req3(player, f3);
+                    }
+                    break;
+                case "수락":
+                    if (Timer.getPlayerInviteTime().containsKey(player)) {
+                        FD.addFriend(player.getUniqueId(), Timer.getPlayerInviteOwner().get(player).getUniqueId());
+                        player.sendMessage("[PlayerPerms] 이제 " + Timer.getPlayerInviteOwner().get(player).getName() + "님과 친구가 되었습니다!");
+                        Timer.getPlayerInviteOwner().get(player).sendMessage("[PlayerPerms] 이제 " + player.getName() + "님과 친구입니다.");
+                        Timer.getPlayerInviteTime().remove(player);
+                        Timer.getPlayerInviteOwner().remove(player);
+                    } else player.sendMessage("[PlayerPerms] 받은 친구 요청이 존재하지 않습니다.");
+                    break;
+                case "거절":
+                    if (Timer.getPlayerInviteTime().containsKey(player)) {
+                        player.sendMessage("[PlayerPerms] " + Timer.getPlayerInviteOwner().get(player).getName() + " 님의 친구 추가 요청을 거절했습니다.");
+                        Timer.getPlayerInviteOwner().get(player).sendMessage("[PlayerPerms] " + player.getName() + " 님이 친구 추가 요청을 거절했습니다.");
+                        Timer.getPlayerInviteTime().remove(player);
+                        Timer.getPlayerInviteOwner().remove(player);
+                    } else player.sendMessage("[PlayerPerms] 받은 친구 요청이 존재하지 않습니다.");
+                    break;
+                }
+            return true;
+        } catch (Exception e) {
+            Player player = (Player) sender;
+            player.sendMessage(ChatColor.RED + "[PlayerPerms] 오류가 발생했습니다");
+            player.sendMessage(ChatColor.YELLOW + "[PlayerPerms] 오류 로그: " + e + "\n[PlayerPerms] 오류코드: 0x14");
+        }
+        return false;
+    }
+
+    public void req(Player player, Player f) {
+        try {
+            Timer.getPlayerInviteOwner().put(f, player);
+            Timer.getPlayerInviteTime().put(f, 60);
+            player.sendMessage(ChatColor.GREEN + "[PlayerPerms] 해당 플레이어에게 성공적으로 친구 요청을 보냈습니다.");
+            f.sendMessage(ChatColor.GREEN + player.getName() + "님이 친구 추가 요청을 보냈습니다. 60초 이내에 응답해주세요. (/친구 수락/거절)");
+        } catch (Exception e) {
+            player.sendMessage(ChatColor.RED + "[PlayerPerm] 친구 추가 요청을 보내는중에 오류가 발생했습니다.");
+                player.sendMessage(ChatColor.YELLOW + "[PlayerPerm] 오류 로그: " + e + "\n[PlayerPerm] 오류 코드: 0x15");
+        }
+    }
+
+    public void req2(Player player, Player f2) {
+        try {
+            Timer.getPlayerInviteOwner().put(f2, player);
+            Timer.getPlayerInviteTime().put(f2, 60);
+            player.sendMessage(ChatColor.GREEN + "해당 플레이어에게 성공적으로 친구 요청을 보냈습니다.");
+            f2.sendMessage(ChatColor.GREEN + player.getName() + "님이 친구 추가 요청을 보냈습니다. 60초 이내에 응답해주세요. (/친구 수락/거절)");
+        } catch (Exception e) {
+            player.sendMessage(ChatColor.RED + "[PlayerPerm] 친구 추가 요청을 보내는중에 오류가 발생했습니다.");
+            player.sendMessage(ChatColor.YELLOW + "[PlayerPerm] 오류 로그: " + e + "\n[PlayerPerm] 오류 코드: 0x15");
+        }
+    }
+
+    public void req3(Player player, Player f3) {
+        try {
+            Timer.getPlayerInviteOwner().put(f3, player);
+            Timer.getPlayerInviteTime().put(f3, 60);
+            player.sendMessage(ChatColor.GREEN + "해당 플레이어에게 성공적으로 친구 요청을 보냈습니다.");
+            f3.sendMessage(ChatColor.GREEN + player.getName() + "님이 친구 추가 요청을 보냈습니다. 60초 이내에 응답해주세요. (/친구 수락/거절)");
+        } catch (Exception e) {
+            player.sendMessage(ChatColor.RED + "[PlayerPerm] 친구 추가 요청을 보내는중에 오류가 발생했습니다.");
+            player.sendMessage(ChatColor.YELLOW + "[PlayerPerm] 오류 로그: " + e + "\n[PlayerPerm] 오류 코드: 0x15");
+        }
+    }
+}
---
+++ src/com/songro/commands/PlayerInfo.java
@@ -0,0 +1,72 @@
+package com.songro.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class PlayerInfo implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender send, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+        if (send.isOp()) {
+            try {
+                Player player = (Player) send;
+                Player target = Bukkit.getPlayer(args[1]);
+                boolean isOnline;
+                var chatcolor = ChatColor.GREEN;
+                var env = target.getWorld().getEnvironment();
+                String pev = null;
+
+                assert target != null;
+                if(target.getPing() >= 1) chatcolor = ChatColor.GREEN;
+                if(target.getPing() > 100) chatcolor = ChatColor.YELLOW;
+                if(target.getPing() > 450) chatcolor = ChatColor.GOLD;
+                if(target.getPing() > 500) chatcolor = ChatColor.RED;
+                if(target.getWorld().getEnvironment() == World.Environment.NORMAL) pev = "오버월드";
+                if(target.getWorld().getEnvironment() == World.Environment.NETHER) pev = "네더";
+                if(target.getWorld().getEnvironment() == World.Environment.THE_END) pev = "엔드";
+
+                if (args[0].equals("플레이어")) {
+                    if (target == null) {
+                        player.sendMessage(ChatColor.RED + "[PlayerPerms] 플레이어가 존재하지 않거나, 서버에 들어온적이 없습니다.");
+                    } else {
+                        isOnline = Bukkit.getOnlinePlayers().contains(target);
+                        player.sendMessage(ChatColor.GREEN + "[PlayerPerms] " + target.getDisplayName() + " 님의 정보");
+                        player.sendMessage(ChatColor.DARK_GRAY + "===============================");
+                        if (!isOnline) player.sendMessage( ChatColor.DARK_GRAY + ": " + ChatColor.DARK_GRAY + "오프라인 ⏺");
+                        else player.sendMessage(ChatColor.DARK_GRAY + ": " + ChatColor.GREEN + "온라인 ⏺");
+                        player.sendMessage(ChatColor.DARK_GRAY + ": " + ChatColor.RED + "♥ 체력: " + target.getHealth());
+                        player.sendMessage(ChatColor.DARK_GRAY + ": " + ChatColor.AQUA + "✧ 경험치: " + target.getExp());
+                        if(target.getBedSpawnLocation() == null) {
+                            player.sendMessage(ChatColor.DARK_GRAY + ": " + ChatColor.DARK_GRAY + "스폰 위치: 존재하지 않음");
+                        } else player.sendMessage(ChatColor.DARK_GRAY + ": " + ChatColor.GREEN + "스폰 위치: X: " + target.getBedSpawnLocation().getX() + " ,Y: " + target.getBedSpawnLocation().getY() + " ,Z: " + target.getBedSpawnLocation().getZ());
+                        player.sendMessage(ChatColor.DARK_GRAY + ": " + chatcolor + "현재 핑: " + target.getPing() + "ms");
+                        player.sendMessage(ChatColor.DARK_GRAY + ": \uD83D\uDDE1 " + ChatColor.DARK_RED + "마지막으로 받은 데미지: " + target.getLastDamage());
+                        if(pev == "오버월드") {
+                            player.sendMessage(ChatColor.DARK_GRAY + ": " + ChatColor.WHITE  + "현재 '" + ChatColor.GREEN + pev + ChatColor.WHITE +"' 에 있는중");
+                        } else if(pev == "네더") {
+                            player.sendMessage(ChatColor.DARK_GRAY + ": " + ChatColor.WHITE  + "현재 '" + ChatColor.RED + pev + ChatColor.WHITE + "' 에 있는중");
+                        } else if(pev == "엔드") {
+                            player.sendMessage(ChatColor.DARK_GRAY + ": " + ChatColor.WHITE + "현재 '" + ChatColor.GRAY + pev + ChatColor.WHITE + "' 에 있는중");
+                        }
+                        player.sendMessage(String.format(ChatColor.DARK_GRAY + ": " + ChatColor.GRAY + "현재 위치: X: " + target.getLocation().getX() + " ,Y: " + target.getLocation().getY() + " ,Z: " + target.getLocation().getZ(), 2f));
+                    }
+                }
+                return true;
+            } catch (Exception e) {
+                Player player = (Player) send;
+                player.sendMessage(ChatColor.RED + "[PlayerPerms] 오류가 발생했습니다");
+                player.sendMessage(ChatColor.YELLOW + "[PlayerPerms] 오류 로그: " + e + "\n[PlayerPerms] 오류코드: 0x13");
+                return false;
+            }
+        } else {
+            Player player = (Player) send;
+            player.sendMessage(ChatColor.RED + "[PlayerPerms] 이 명령어는 OP만 사용 가능합니다.");
+        }
+        return false;
+    }
+}
