f96a934e5e2c015750816ae4779f0ac27dc1ac4d
songro_ <sdbb2q@gmail.com>
2023-03-13 16:15:22 +0900

    Fix space

--- src/com/songro/commands/QuietMessage.java
+++ src/com/songro/commands/QuietMessage.java
@@ -13,6 +13,7 @@ public class QuietMessage implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
         Player player = (Player)sender;
         Player target = Bukkit.getPlayer(args[0]);
+        StringBuilder builder = new StringBuilder();
         String msg = args[1];
 
         try {
@@ -32,12 +33,29 @@ public class QuietMessage implements CommandExecutor {
             if (!Bukkit.getOnlinePlayers().contains(target))
                 player.sendMessage(ChatColor.YELLOW + "[PlayerPerms] '" + target.getDisplayName() +"' 플레이어는 온라인이 아닙니다.");
             try {
-                if(target.equals(player)) {
-                    player.sendMessage( ChatColor.YELLOW + "[PlayerPerms] 자기 자신에게 귓속말을 보낼수 없습니다.");
+                if(args.length == 1) {
+                    //if(target.equals(player)) {
+                    //    player.sendMessage( ChatColor.YELLOW + "[PlayerPerms] 자기 자신에게 귓속말을 보낼수 없습니다.");
+                    //} else {
+                    target.sendMessage(ChatColor.GRAY + "[PlayerPerms] 새로운 메세지를 받았습니다.\n• " + player.getDisplayName() + "(이)가 보낸 메세지: " + msg);
+                    player.sendMessage(ChatColor.GREEN + "[PlayerPerms] " + target.getDisplayName() + "에게 성공적으로 " + msg + "라고 메세지를 보냈습니다.");
+                    return true;
+                    //}
                 } else {
-                target.sendMessage(ChatColor.GRAY + "[PlayerPerms] 새로운 메세지를 받았습니다.\n• " + player.getDisplayName() + "(이)가 보낸 메세지: " + msg);
-                player.sendMessage(ChatColor.GREEN + "[PlayerPerms] " + target.getDisplayName() + "에게 성공적으로 " + msg + "라고 메세지를 보냈습니다.");
-                return true;
+                    for(int i = 1; i < args.length; i++) {
+                        builder.append(args[i]);
+                        builder.append(" ");
+                    }
+
+                    String finalMessage = builder.toString();
+                    finalMessage = finalMessage.stripTrailing();
+                    //if(target.equals(player)) {
+                    //    player.sendMessage( ChatColor.YELLOW + "[PlayerPerms] 자기 자신에게 귓속말을 보낼수 없습니다.");
+                    //} else {
+                    target.sendMessage(ChatColor.GRAY + "[PlayerPerms] 새로운 메세지를 받았습니다.\n• " + player.getDisplayName() + "(이)가 보낸 메세지: " + finalMessage);
+                    player.sendMessage(ChatColor.GREEN + "[PlayerPerms] " + target.getDisplayName() + "에게 성공적으로 " + finalMessage + "라고 메세지를 보냈습니다.");
+                    return true;
+
                 }
 
 
@@ -51,7 +69,5 @@ public class QuietMessage implements CommandExecutor {
             player.sendMessage(ChatColor.YELLOW + "[PlayerPerms] 오류 로그: " + e + "\n[PlayerPerms] 오류코드: 0x18");
             return false;
         }
-
-        return false;
     }
 }
