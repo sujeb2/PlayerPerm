d2dcc5d1daa0b3c0bd47b86e1238e4c4cbcf322b
songro_ <sdbb2q@gmail.com>
2023-03-19 08:56:35 +0900

    Changed playerinfo command to just typing names

--- src/com/songro/commands/PlayerInfoNormal.java
+++ src/com/songro/commands/PlayerInfoNormal.java
@@ -17,7 +17,7 @@ public class PlayerInfoNormal implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender send, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
             try {
                 Player player = (Player) send;
-                Player target = Bukkit.getPlayer(args[1]);
+                Player target = Bukkit.getPlayer(args[0]);
                 boolean isOnline;
                 var chatcolor = ChatColor.GREEN;
                 var env = target.getWorld().getEnvironment();
@@ -36,50 +36,26 @@ public class PlayerInfoNormal implements CommandExecutor {
                 if(target.getWorld().getEnvironment() == World.Environment.NETHER) pev = "네더";
                 if(target.getWorld().getEnvironment() == World.Environment.THE_END) pev = "엔드";
 
-                if (args[0].equals("정보")) {
-                    if (target == null) {
-                        player.sendMessage(ChatColor.RED + "[PlayerPerms] 플레이어가 존재하지 않거나, 서버에 들어온적이 없습니다.");
-                    } else {
-                        isOnline = Bukkit.getOnlinePlayers().contains(target);
-                        player.sendMessage(ChatColor.GREEN + "[PlayerPerms] " + target.getDisplayName() + " 님의 정보");
-                        player.sendMessage(ChatColor.DARK_GRAY + "===============================");
-                        if (!isOnline) player.sendMessage( ChatColor.DARK_GRAY + ": " + ChatColor.DARK_GRAY + "⏺ 오프라인");
-                        else player.sendMessage(ChatColor.DARK_GRAY + ": " + ChatColor.GREEN + "⏺ 온라인");
-                        //player.sendMessage(ChatColor.DARK_GRAY + ": " + ChatColor.RED + "♥ 체력: " + target.getHealth());
-                        player.sendMessage(ChatColor.DARK_GRAY + ": " + ChatColor.AQUA + "✧ 경험치: " + target.getExp());
-                        player.sendMessage(ChatColor.DARK_GRAY + ": " + chatcolor + "현재 핑: " + target.getPing() + "ms");
-                        //player.sendMessage(ChatColor.DARK_GRAY + ": \uD83D\uDDE1 " + ChatColor.DARK_RED + "마지막으로 받은 데미지: " + (Math.round(target.getLastDamage())));
-                        String unixTimeStamp = String.valueOf(target.getLastPlayed());
-                        long timestamp = Long.parseLong(unixTimeStamp);
-                        SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss");
-                        Date date = new Date();
-                        date.setTime(timestamp);
+                if (target == null) {
+                    player.sendMessage(ChatColor.RED + "[PlayerPerms] 플레이어가 존재하지 않거나, 서버에 들어온적이 없습니다.");
+                } else {
+                    isOnline = Bukkit.getOnlinePlayers().contains(target);
+                    player.sendMessage(ChatColor.GREEN + "[PlayerPerms] " + target.getDisplayName() + " 님의 정보");
+                    player.sendMessage(ChatColor.DARK_GRAY + "===============================");
+                    if (!isOnline) player.sendMessage( ChatColor.DARK_GRAY + ": " + ChatColor.DARK_GRAY + "⏺ 오프라인");
+                    else player.sendMessage(ChatColor.DARK_GRAY + ": " + ChatColor.GREEN + "⏺ 온라인");
+                    //player.sendMessage(ChatColor.DARK_GRAY + ": " + ChatColor.RED + "♥ 체력: " + target.getHealth());
+                    player.sendMessage(ChatColor.DARK_GRAY + ": " + ChatColor.AQUA + "✧ 경험치: " + target.getExp());
+                    player.sendMessage(ChatColor.DARK_GRAY + ": " + chatcolor + "현재 핑: " + target.getPing() + "ms");
+                    //player.sendMessage(ChatColor.DARK_GRAY + ": \uD83D\uDDE1 " + ChatColor.DARK_RED + "마지막으로 받은 데미지: " + (Math.round(target.getLastDamage())));
+                    String unixTimeStamp = String.valueOf(target.getLastPlayed());
+                    long timestamp = Long.parseLong(unixTimeStamp);
+                    SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss");
+                    Date date = new Date();
+                    date.setTime(timestamp);
 
-                        String Datetime = sdf.format(date);
-                        player.sendMessage(ChatColor.DARK_GRAY + ": " + "마지막 접속 시간: " + ChatColor.GRAY + Datetime);
-                    }
-                } else if (args[0].equals("info")){
-                    if (target == null) {
-                        player.sendMessage(ChatColor.RED + "[PlayerPerms] 플레이어가 존재하지 않거나, 서버에 들어온적이 없습니다.");
-                    } else {
-                        isOnline = Bukkit.getOnlinePlayers().contains(target);
-                        player.sendMessage(ChatColor.GREEN + "[PlayerPerms] " + target.getDisplayName() + " 님의 정보");
-                        player.sendMessage(ChatColor.DARK_GRAY + "===============================");
-                        if (!isOnline) player.sendMessage( ChatColor.DARK_GRAY + ": " + ChatColor.DARK_GRAY + "⏺ 오프라인");
-                        else player.sendMessage(ChatColor.DARK_GRAY + ": " + ChatColor.GREEN + "⏺ 온라인");
-                        //player.sendMessage(ChatColor.DARK_GRAY + ": " + ChatColor.RED + "♥ 체력: " + target.getHealth());
-                        player.sendMessage(ChatColor.DARK_GRAY + ": " + ChatColor.AQUA + "✧ 경험치: " + target.getExp());
-                        player.sendMessage(ChatColor.DARK_GRAY + ": " + chatcolor + "현재 핑: " + target.getPing() + "ms");
-                        //player.sendMessage(ChatColor.DARK_GRAY + ": \uD83D\uDDE1 " + ChatColor.DARK_RED + "마지막으로 받은 데미지: " + (Math.round(target.getLastDamage())));
-                        String unixTimeStamp = String.valueOf(target.getLastPlayed());
-                        long timestamp = Long.parseLong(unixTimeStamp);
-                        SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss");
-                        Date date = new Date();
-                        date.setTime(timestamp);
-
-                        String Datetime = sdf.format(date);
-                        player.sendMessage(ChatColor.DARK_GRAY + ": " + "마지막 접속 시간: " + ChatColor.GRAY + Datetime);
-                    }
+                    String Datetime = sdf.format(date);
+                    player.sendMessage(ChatColor.DARK_GRAY + ": " + "마지막 접속 시간: " + ChatColor.GRAY + Datetime);
                 }
                 return true;
             } catch (Exception e) {
--- src/com/songro/commands/op/PlayerInfo.java
+++ src/com/songro/commands/op/PlayerInfo.java
@@ -18,9 +18,10 @@ public class PlayerInfo implements CommandExecutor {
         if (send.isOp()) {
             try {
                 Player player = (Player) send;
-                Player target = Bukkit.getPlayer(args[1]);
+                Player target = Bukkit.getPlayer(args[0]);
                 boolean isOnline;
                 var chatcolor = ChatColor.GREEN;
+                assert target != null;
                 var env = target.getWorld().getEnvironment();
                 String pev = null;
 
@@ -28,7 +29,6 @@ public class PlayerInfo implements CommandExecutor {
                 var y = target.getLocation().getY();
                 var z = target.getLocation().getZ();
 
-                assert target != null;
                 if(target.getPing() >= 1) chatcolor = ChatColor.GREEN;
                 if(target.getPing() > 100) chatcolor = ChatColor.YELLOW;
                 if(target.getPing() > 450) chatcolor = ChatColor.GOLD;
@@ -38,7 +38,6 @@ public class PlayerInfo implements CommandExecutor {
                 if(target.getWorld().getEnvironment() == World.Environment.THE_END) pev = "엔드";
 
                 String s1 = ChatColor.DARK_GRAY + ": " + ChatColor.GRAY + "현재 위치: X: " + (Math.round(x)) + " ,Y: " + (Math.round(y)) + " ,Z: " + (Math.round(z));
-                if (args[0].equals("정보")) {
                     if (target == null) {
                         player.sendMessage(ChatColor.RED + "[PlayerPerms] 플레이어가 존재하지 않거나, 서버에 들어온적이 없습니다.");
                     } else {
@@ -65,7 +64,7 @@ public class PlayerInfo implements CommandExecutor {
                         String unixTimeStamp = String.valueOf(target.getLastPlayed());
 
                         long timestamp = Long.parseLong(unixTimeStamp);
-                        SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss");
+                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
                         Date date = new Date();
                         date.setTime(timestamp);
 
@@ -73,43 +72,6 @@ public class PlayerInfo implements CommandExecutor {
                         player.sendMessage(ChatColor.DARK_GRAY + ": " + "마지막 접속 시간: " + ChatColor.GRAY + Datetime);
                         player.sendMessage(ChatColor.DARK_GRAY + ": " + ChatColor.GRAY + "엔티티 ID " + target.getEntityId());
                         player.sendMessage(ChatColor.YELLOW + "[PlayerPerms] 현재 OP 상태로 " + target.getDisplayName() + "의 정보를 보고 있습니다.\n[PlayerPerms] 이 정보는 오직 OP만 알고 있어야합니다.");
-                    }
-                } else if (args[0].equals("info")){
-                    if (target == null) {
-                        player.sendMessage(ChatColor.RED + "[PlayerPerms] 플레이어가 존재하지 않거나, 서버에 들어온적이 없습니다.");
-                    } else {
-                        isOnline = Bukkit.getOnlinePlayers().contains(target);
-                        player.sendMessage(ChatColor.GREEN + "[PlayerPerms] " + target.getDisplayName() + " 님의 정보");
-                        player.sendMessage(ChatColor.DARK_GRAY + "===============================");
-                        if (!isOnline) player.sendMessage( ChatColor.DARK_GRAY + ": " + ChatColor.DARK_GRAY + "⏺ 오프라인");
-                        else player.sendMessage(ChatColor.DARK_GRAY + ": " + ChatColor.GREEN + "⏺ 온라인");
-                        player.sendMessage(ChatColor.DARK_GRAY + ": " + ChatColor.RED + "♥ 체력: " + target.getHealth());
-                        player.sendMessage(ChatColor.DARK_GRAY + ": " + ChatColor.AQUA + "✧ 경험치: " + target.getExp());
-                        if(target.getBedSpawnLocation() == null) {
-                            player.sendMessage(ChatColor.DARK_GRAY + ": " + ChatColor.DARK_GRAY + "스폰 위치: 존재하지 않음");
-                        } else player.sendMessage(ChatColor.DARK_GRAY + ": " + ChatColor.GREEN + "스폰 위치: X: " + target.getBedSpawnLocation().getX() + " ,Y: " + target.getBedSpawnLocation().getY() + " ,Z: " + target.getBedSpawnLocation().getZ());
-                        player.sendMessage(ChatColor.DARK_GRAY + ": " + chatcolor + "현재 핑: " + target.getPing() + "ms");
-                        player.sendMessage(ChatColor.DARK_GRAY + ": \uD83D\uDDE1 " + ChatColor.DARK_RED + "마지막으로 받은 데미지: " + (Math.round(target.getLastDamage())));
-                        if(pev == "오버월드") {
-                            player.sendMessage(ChatColor.DARK_GRAY + ": " + ChatColor.GRAY + "현재 '" + ChatColor.GREEN + pev + ChatColor.GRAY + "' 에 있는중");
-                        } else if(pev == "네더") {
-                            player.sendMessage(ChatColor.DARK_GRAY + ": " + ChatColor.GRAY + "현재 '" + ChatColor.RED + pev + ChatColor.GRAY + "' 에 있는중");
-                        } else if(pev == "엔드") {
-                            player.sendMessage(ChatColor.DARK_GRAY + ": " + ChatColor.GRAY + "현재 '" + ChatColor.DARK_GRAY + pev + ChatColor.GRAY + "' 에 있는중");
-                        }
-                        String unixTimeStamp = String.valueOf(target.getLastPlayed());
-
-                        long timestamp = Long.parseLong(unixTimeStamp);
-                        SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss");
-                        Date date = new Date();
-                        date.setTime(timestamp);
-
-                        String Datetime = sdf.format(date);
-                        player.sendMessage(s1);
-                        player.sendMessage(ChatColor.DARK_GRAY + ": " + "마지막 접속 시간: " + ChatColor.GRAY + Datetime);
-                        player.sendMessage(ChatColor.DARK_GRAY + ": " + ChatColor.GRAY + "엔티티 ID " + target.getEntityId());
-                        player.sendMessage(ChatColor.YELLOW + "[PlayerPerms] 현재 OP 상태로 " + target.getDisplayName() + "의 정보를 보고 있습니다.\n[PlayerPerms] 이 정보는 오직 OP만 알고 있어야합니다.");
-                    }
                 }
                 return true;
             } catch (Exception e) {
