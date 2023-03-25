249528b9f2d3bf965d9df417e8fd836725d2e2a1
songro_ <sdbb2q@gmail.com>
2023-03-25 17:25:01 +0900

    Bunch of if else

--- src/com/songro/event/KillHeadDrop.java
+++ src/com/songro/event/KillHeadDrop.java
@@ -10,6 +10,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.logging.Logger;
 
 public class KillHeadDrop implements Listener {
@@ -18,25 +19,51 @@ public class KillHeadDrop implements Listener {
 
     @EventHandler
     public void onKill(PlayerDeathEvent e) {
-        if(e.getEntity().getKiller().hasPermission("perks.plus") || e.getEntity().getKiller().hasPermission("perks.pp") || e.getEntity().getKiller().hasPermission("perks.op") || e.getEntity().getKiller().hasPermission("perks.ultra") || e.getEntity().getKiller() != null) {
-            try {
-                if (e.getEntity().getKiller().hasPermission("perks.plus") || e.getEntity().getKiller().hasPermission("perks.pp") || e.getEntity().getKiller().hasPermission("perks.op") || e.getEntity().getKiller().hasPermission("perks.ultra") || e.getEntity().getKiller() != null) {
+        try {
+            if (e.getEntity() != null) {
+                if (Objects.requireNonNull(e.getEntity().getKiller()).hasPermission("perks.plus") || e.getEntity().getKiller().hasPermission("perks.pp") || e.getEntity().getKiller().hasPermission("perks.op") || e.getEntity().getKiller().hasPermission("perks.ultra") || (e.getEntity().getKiller() != null)) {
+                    try {
+                        if (e.getEntity().getKiller().hasPermission("perks.plus") || e.getEntity().getKiller().hasPermission("perks.pp") || e.getEntity().getKiller().hasPermission("perks.op") || e.getEntity().getKiller().hasPermission("perks.ultra") || e.getEntity().getKiller() != null) {
+                            ItemStack item = new ItemStack(Material.LEGACY_SKULL_ITEM, 1, (short) 3);
+                            SkullMeta sm = (SkullMeta) item.getItemMeta();
+                            ArrayList<String> lore = new ArrayList<>();
+                            assert sm != null;
+                            sm.setOwner((e.getEntity().getName()));
+                            if (e.getEntity().getKiller() != null) {
+                                lore.add(e.getEntity().getKiller() + "(이)가 죽인 " + e.getEntity().getDisplayName() + ChatColor.GRAY + "의 머리이다.");
+                            } else {
+                                lore.add(ChatColor.GRAY + e.getEntity().getDisplayName() + ChatColor.GRAY + "의 머리이다.");
+                            }
+                            sm.setLore(lore);
+                            item.setItemMeta(sm);
+
+                            e.getDrops().add(item);
+                        }
+                    } catch (NullPointerException npe) {
+                        ItemStack item = new ItemStack(Material.LEGACY_SKULL_ITEM, 1, (short) 3);
+                        SkullMeta sm = (SkullMeta) item.getItemMeta();
+                        ArrayList<String> lore = new ArrayList<>();
+                        assert sm != null;
+                        sm.setOwner((e.getEntity().getName()));
+                        lore.add(e.getEntity().getDisplayName() + ChatColor.GRAY + "의 머리이다.");
+                        sm.setLore(lore);
+                        item.setItemMeta(sm);
+
+                        e.getDrops().add(item);
+                    }
+                } else {
                     ItemStack item = new ItemStack(Material.LEGACY_SKULL_ITEM, 1, (short) 3);
                     SkullMeta sm = (SkullMeta) item.getItemMeta();
                     ArrayList<String> lore = new ArrayList<>();
                     assert sm != null;
                     sm.setOwner((e.getEntity().getName()));
-                    if (e.getEntity().getKiller() != null) {
-                        lore.add(e.getEntity().getKiller() + "(이)가 죽인 " + e.getEntity().getDisplayName() + ChatColor.GRAY + "의 머리이다.");
-                    } else {
-                        lore.add(ChatColor.GRAY + e.getEntity().getDisplayName() + ChatColor.GRAY + "의 머리이다.");
-                    }
+                    lore.add(e.getEntity().getDisplayName() + ChatColor.GRAY + "의 머리이다.");
                     sm.setLore(lore);
                     item.setItemMeta(sm);
 
                     e.getDrops().add(item);
                 }
-            } catch (NullPointerException npe) {
+            } else {
                 ItemStack item = new ItemStack(Material.LEGACY_SKULL_ITEM, 1, (short) 3);
                 SkullMeta sm = (SkullMeta) item.getItemMeta();
                 ArrayList<String> lore = new ArrayList<>();
@@ -48,8 +75,17 @@ public class KillHeadDrop implements Listener {
 
                 e.getDrops().add(item);
             }
-        } else {
-            log.info("player doesn't have permission");
+        } catch (NullPointerException npe2) {
+            ItemStack item = new ItemStack(Material.LEGACY_SKULL_ITEM, 1, (short) 3);
+            SkullMeta sm = (SkullMeta) item.getItemMeta();
+            ArrayList<String> lore = new ArrayList<>();
+            assert sm != null;
+            sm.setOwner((e.getEntity().getName()));
+            lore.add(e.getEntity().getDisplayName() + ChatColor.GRAY + "의 머리이다.");
+            sm.setLore(lore);
+            item.setItemMeta(sm);
+
+            e.getDrops().add(item);
         }
     }
 }
